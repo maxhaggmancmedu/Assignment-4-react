1d7425ba39e621058ec99242b9f0cf26bd8acab0
Max Häggman <max.haggman@edu.cmeducations.se>
2023-02-23 13:25:21 +0100

    first commit graphQL

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState, useEffect } from 'react'
 import CharacterComponent from './Characters'
 import Nav from './Nav'
+import fetchQL from './fetchQL';
 
 function App() {
   const [characters, setCharacters] = useState([]);
   
   useEffect(() => {
-    fetch('https://api.disneyapi.dev/characters?page=43')
+    fetchQL(`
+    {
+      characters(page: 43) {
+        items {
+          _id
+          name
+          imageUrl
+        }
+      }
+    }
+  `)
     .then((res) => {
       return res.json()
     })
     .then((data) => {
       console.log(data)
-      setCharacters(data.data)
+      setCharacters(data.data.characters.items)
     })
   }, [])
 
@@ -23,7 +34,7 @@ function App() {
       <Nav />
       <div className='character-component'>
         {characters.map((character) => {
-          return <CharacterComponent key={[0]._id} {...character}></CharacterComponent>
+          return <CharacterComponent key={character._id} {...character}></CharacterComponent>
         })}
       </div>
     </div>
---
+++ src/fetchQL.js
@@ -0,0 +1,14 @@
+export default (query) => {
+    return fetch('https://api.disneyapi.dev/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          // now: new Date().toISOString(),
+        },
+      })
+    })
+  }
